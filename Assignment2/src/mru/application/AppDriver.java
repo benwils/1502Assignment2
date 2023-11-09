d46bf7c3fa5a63777d4e46feff84bd05546ce92d
benwils <112783613+benwils@users.noreply.github.com>
2023-11-08 17:05:40 -0700

    Add files via upload

---
+++ Assignment2/src/mru/application/AppDriver.java
@@ -0,0 +1,18 @@
+package mru.application;
+
+import java.util.ArrayList;
+
+import mru.model.Figures;
+
+public class AppDriver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ArrayList<Figures> toys = new ArrayList<>();
+		
+		
+
+	}
+
+}
